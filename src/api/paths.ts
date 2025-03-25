0d27230f95d3975fb536ff0852d50864b421c291
Arbaj Ansari <127431775+ArbajAnsari19@users.noreply.github.com>
2025-03-25 23:48:59 +0530

    Update paths.ts

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -1,4 +1,4 @@
-const URL = "https://backend.wavezgoa.com"; 
+const URL = "https://www.backend.wavezgoa.com"; 
 // const URL = "http://localhost:8000"; //local server
 const userBaseURL = URL + "/user";
 const customer = URL + "/customer";
